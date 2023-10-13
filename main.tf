bcb4351e5e5a282d9ff55840496effa76df62166
Rahil Hussain <84090207+Rahil-hussain@users.noreply.github.com>
2023-10-13 13:37:53 -0500

    Update main.tf
    test kaprenter

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ data "aws_ecrpublic_authorization_token" "token" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = "datateamCluster01"
+  name   = "dataClustertest1"
   region = "us-west-2"
 
   vpc_cidr = "10.0.0.0/16"
